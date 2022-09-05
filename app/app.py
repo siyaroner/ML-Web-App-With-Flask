09f066519e05c46f451ed1a79f604b14389b5afa
Şiyar Öner <siyaroner@gmail.com>
2022-09-05 07:17:37 +0300

    app.py updated

--- app/app.py
+++ app/app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+import numpy as np
+from flask import Flask, render_template,request
+
+# from joblib import load
 
 app = Flask(__name__)
-
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
 def hello_world():
-    return "Hello, World!"
+    request_type_str=request.method
+    if request_type_str == 'GET':
+        return render_template("index.html")
+    # else:
+    #     text=request.form["text"]
+    #     return text.upper()
